3a6149778fb1e3e2a4454ee4e0411119f04f216e
Chioma <chiomauchenwosu@gmail.com>
2023-04-24 17:29:56 +0100

    Chioma: Created MVC files

---
+++ src/views/App.jsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-blue-700">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
