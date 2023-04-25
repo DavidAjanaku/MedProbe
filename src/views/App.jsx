438498b7d6c3aa2ac58342e000fb9a665199c092
David Ajanaku <user@David-Ajanaku.local>
2023-04-25 12:27:17 +0100

    David: Router

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,17 +1,26 @@
 import "../App.css";
+import {BrowserRouter as Router,Routes,Route, Link} from "react-router-dom";
+import Home from "./HomePage"
 import SignUpPage from "./SignUpPage";
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-blue-700">Hello World</h1>
-        <div className="font-acumin-pro font-regular">Regular</div>
-        <div className="font-acumin-pro-italic font-regular">Italic</div>
-        <div className="font-acumin-pro font-bold">Bold</div>
-        <div className="font-acumin-pro-bold-italic font-bold">Bold Italic</div>
-      </div>
-      <SignUpPage />
+    
+     
+      <Router >
+      <nav className="justify-between...  bg-indigo-500 ...">
+        <Link to ="/">Home</Link>
+        <Link to ="/Signup">Sign Up</Link>
+      
+
+      </nav>
+        <Routes>
+          <Route  path="/" element={<Home/>}/>
+          <Route  path="/Signup" element={<SignUpPage/>}/>
+
+        </Routes>
+      </Router>
     </>
   );
 }
