6776fb381c7081e4a607a34b0aec169a037def7e
Chioma <chiomauchenwosu@gmail.com>
2023-05-06 19:44:29 +0100

    Chioma: Email and password styling

--- src/views/SignUpPage.jsx
+++ src/views/SignUpPage.jsx
@@ -20,39 +20,81 @@ export default function SignUpPage() {
         </div>
 
         {/* 2. FORM CONTAINER */}
-        <div className="main-form-container my-44">
-          <div className="flex flex-col items-center">
-            <h1 className="text-4xl font-extrabold text-medium-blue">
-              Welcome Back!
-            </h1>
-            <p className="text-text-grey">
-              To stay connected with us, please login with your personal info.
-            </p>
-          </div>
-          <form className="flex flex-col items-center my-20">
-            <input type="email" placeholder="Email" required />
-            <label>Email</label>
+        <div className="main-form-container my-40 mx-44 border-4 w-3/5">
+          <div className="">
+            <div className="flex flex-col items-center">
+              <h1 className="text-4xl font-extrabold text-medium-blue">
+                Welcome Back!
+              </h1>
+              <p className="text-text-grey text-center">
+                To stay connected with us, please login with your personal info.
+              </p>
+            </div>
 
-            <input type="password" placeholder="Password" required />
-            <label>Password</label>
+            {/* 3. MAIN FORM  */}
+            <form className="flex flex-col my-20 border-4">
+              <span className="block text-sm font-medium text-slate-700 ">
+                Email
+              </span>
+              <input
+                type="email"
+                required
+                placeholder="you@example.com"
+                className="mt-1 block w-full my-4 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              />
 
-            <div className="flex justify-between gap-10">
-              <div>
-                <input type="checkbox" id="checkbox" />
-                <label>I agree to the terms and conditions</label>
+              <span className="block text-sm font-medium text-slate-700 ">
+                Password
+              </span>
+              <input
+                type="password"
+                required
+                className="mt-1 my-4 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+          "
+              />
+
+              <div className="flex justify-between gap-10 items-center">
+                <div>
+                  <input type="checkbox" id="checkbox" />
+                  <label>I agree to the terms and conditions</label>
+                </div>
+                <button className="rounded-md bg-medium-blue py-3 px-6 text-white text-sm flex items-center">
+                  Sign In
+                  <svg
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6 15L10 10L6 5"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
               </div>
-              <button>Sign In</button>
-            </div>
 
-            <p>Forgot password?</p>
-            <p>Connect with social Account</p>
-            <div className="social-media-icons flex flex-col-3 gap-10">
-              <p>Google</p>
-              <p>Apple</p>
-              <p>GitHub</p>
-            </div>
-            <button>Register here</button>
-          </form>
+              <p className="">Forgot password?</p>
+              <p>Connect with social Account</p>
+              <div className="social-media-icons flex flex-col-3 gap-10">
+                <p>Google</p>
+                <p>Apple</p>
+                <p>GitHub</p>
+              </div>
+              <button>Register here</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
