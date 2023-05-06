cf819262316f24c40b3d79a341dc77ea2a09ba88
David Ajanaku <user@David-Ajanaku.local>
2023-05-06 23:09:44 +0100

    done

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -4,10 +4,10 @@ export default function SearchSection() {
       <section className="box-content text-center mt-0 mb-0 ml-auto mr-auto  max-w-2xl">
         <img
           src="/public/assets/illustrations/search (4).png"
-          className="w-32  mt-14 mx-auto mb-12"
+          className="w-12  mt-14 mx-auto mb-8"
           alt="logo"
         />
-        <div className="companyName text-5xl">MedProbe</div>
+        <div className="companyName text-3xl flex items-center justify-center"><p className="font-medium bg-medium-blue text-white rounded p-2">med</p> <span className=" font-medium ml-[-8px]">Probe</span></div>
       </section>
     </>
   );
