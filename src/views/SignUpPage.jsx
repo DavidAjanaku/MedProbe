ff9f1edc0533eca9202fe8e72cdf9824f2d7542e
Chioma <chiomauchenwosu@gmail.com>
2023-05-06 20:15:23 +0100

    Chioma: Finished Sign In Page

--- src/views/SignUpPage.jsx
+++ src/views/SignUpPage.jsx
@@ -8,8 +8,8 @@ export default function SignUpPage() {
             <img
               src="/assets/illustrations/search (4).png"
               alt="logo"
-              width="40px"
-              height="40px"
+              width="30px"
+              height="30px"
             />
             <p className="text-2xl text-text-blue">medProbe</p>
           </div>
@@ -20,7 +20,7 @@ export default function SignUpPage() {
         </div>
 
         {/* 2. FORM CONTAINER */}
-        <div className="main-form-container my-40 mx-44 border-4 w-3/5">
+        <div className="main-form-container my-16 mx-44 w-3/5">
           <div className="">
             <div className="flex flex-col items-center">
               <h1 className="text-4xl font-extrabold text-medium-blue">
@@ -32,28 +32,28 @@ export default function SignUpPage() {
             </div>
 
             {/* 3. MAIN FORM  */}
-            <form className="flex flex-col my-20 border-4">
-              <span className="block text-sm font-medium text-slate-700 ">
+            <form className="flex flex-col my-20 ">
+              <span className="block text-sm font-medium text-medium-blue ">
                 Email
               </span>
               <input
                 type="email"
                 required
                 placeholder="you@example.com"
-                className="mt-1 block w-full my-4 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                className="mt-1 block w-full h-12 my-4 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
               disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
               invalid:border-pink-500 invalid:text-pink-600
               focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
               />
 
-              <span className="block text-sm font-medium text-slate-700 ">
+              <span className="block text-sm font-medium  text-medium-blue ">
                 Password
               </span>
               <input
                 type="password"
                 required
-                className="mt-1 my-4 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                className="mt-1 my-4 h-12 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
               focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
               disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
               invalid:border-pink-500 invalid:text-pink-600
@@ -66,7 +66,7 @@ export default function SignUpPage() {
                   <input type="checkbox" id="checkbox" />
                   <label>I agree to the terms and conditions</label>
                 </div>
-                <button className="rounded-md bg-medium-blue py-3 px-6 text-white text-sm flex items-center">
+                <button className="rounded-md bg-medium-blue py-3 px-6 text-white text-sm flex items-center my-4">
                   Sign In
                   <svg
                     className="w-4 h-4 ml-2"
@@ -85,14 +85,37 @@ export default function SignUpPage() {
                 </button>
               </div>
 
-              <p className="">Forgot password?</p>
-              <p>Connect with social Account</p>
-              <div className="social-media-icons flex flex-col-3 gap-10">
-                <p>Google</p>
-                <p>Apple</p>
-                <p>GitHub</p>
+              <a className="text-yellow-300 text-center text-sm my-4" href="#">
+                Forgot password?
+              </a>
+              <p className="text-text-grey text-center text-sm my-4">
+                Connect with social account
+              </p>
+
+              <div className="social-media-icons my-4 flex justify-center gap-14">
+                <img
+                  src="/public/assets/illustrations/googleIcon.png"
+                  alt="google"
+                  width="20px"
+                  height="20px"
+                />
+                <img
+                  src="/public/assets/illustrations/appleLogo.png"
+                  alt="apple"
+                  width="20px"
+                  height="20px"
+                />
+                <img
+                  src="/public/assets/illustrations/facebookLogo2.png"
+                  alt="facebook"
+                  width="20px"
+                  height="20px"
+                />
               </div>
-              <button>Register here</button>
+
+              <button className="border-2 text-sm py-3 my-6 rounded-lg border-black">
+                Register here
+              </button>
             </form>
           </div>
         </div>
