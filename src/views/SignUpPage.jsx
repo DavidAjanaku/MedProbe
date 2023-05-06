561a634d93b5650b917597f08053c1e88e368754
Chioma <chiomauchenwosu@gmail.com>
2023-05-06 14:53:40 +0100

    Chioma: Started Sign Up Page

--- src/views/SignUpPage.jsx
+++ src/views/SignUpPage.jsx
@@ -1,6 +1,20 @@
 export default function SignUpPage() {
   return (
     <div className='font-acumin'>
-      SignUpPage</div>
+      <div className="main-container">
+        <div className="wrapper-container">
+          <div className="main-img-container">
+            <div className="img-wrapper">
+              <img />
+            </div>
+          </div>
+          <div className="main-form-container">
+            <div className="form-wrapper">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
