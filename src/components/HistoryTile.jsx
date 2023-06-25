c7ff84295bb53c5a1c77d2715d0ecddcea3f9384
DavidAjanaku <davidajanaku46163@gmail.com>
2023-06-25 23:15:27 +0100

    David:added icons

--- src/components/HistoryTile.jsx
+++ src/components/HistoryTile.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
+import deleteCan from '/Users/user/Desktop/MyProjects/med-probe/public/assets/illustrations/delete.png'
+import download from '/Users/user/Desktop/MyProjects/med-probe/public/assets/illustrations/download.png'
+import message from '/Users/user/Desktop/MyProjects/med-probe/public/assets/illustrations/messagebubble.png'
+import share from '/Users/user/Desktop/MyProjects/med-probe/public/assets/illustrations/share.png'
 
 export default function HistoryTile(props) {
   return (
     <div>
       <div>
-        <div className='flex  justify-between py-4 px-5 my-4 text-white bg-[#ffffff26;] border-white border-[1px] rounded-md cursor-pointer '>
+        <div className='flex  justify-between py-4 px-5 my-4 text-white bg-[#ffffff26;] border-white border-[1px] rounded-md cursor-pointer items-center '>
           <span>
-            &#128172;
+          <img className='w-5 m4-1' src={message} alt="" />
+
           </span>
-          <span className="overflow-hidden whitespace-nowrap overflow-ellipsis w-[50%]">
+          <span className="overflow-hidden mx-1 whitespace-nowrap overflow-ellipsis w-[50%]">
           {props.title}
           </span>
           <span>
-          &#8595;
+         <img className='w-5 mx-1' src={download} alt="" />
           </span>
           <span>
-          &#x3c;
+          <img className='w-5 mx-1' src={share} alt="" />
+
           </span>
           <span>
-          &#9249;
+         <img className='w-5 mx-1' src={deleteCan} alt="" />
           </span>
         </div>
       </div>
